b268a567415f7752e1021033a75bac44bd8187c9
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-27 05:37:03 +0600

    added serch box

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { FormControl, InputGroup, Button } from 'react-bootstrap';
 
 const Event = () => {
     return (
         <div>
-            <h2>Event</h2>
+            <div className='container'>
+                <InputGroup className="mb-3 mt-5 w-25 ms-auto">
+                    <FormControl
+                        placeholder="search event"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <Button variant="secondary" id="button-addon2">
+                        Search
+                    </Button>
+                </InputGroup>
+            </div>
         </div>
     );
 };
